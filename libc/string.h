137cee6a76391ccf1e2acfdfc45328318f169f25
cth103 <cthdev@email.com>
2018-04-21 22:50:38 +0100

    Update string.h

--- libc/string.h
+++ libc/string.h
@@ -1,5 +1,4 @@
 #define NULL ((void *) 0)
 void *memcpy(void *dest, const void *src, size_t count);
-void *memset(void *dest, char val, size_t count);
 unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count);
-size_t strlen(const char *str);
+int strlen(const char *str);
