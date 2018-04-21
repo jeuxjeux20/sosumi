c7d7753050e0d6a3cf55df17c0d7d20e2dd9c623
cth103 <cthdev@email.com>
2018-04-21 21:08:15 +0100

    Update main.c

--- main.c
+++ main.c
@@ -18,6 +18,13 @@ void clearVideo() {
 }
 void main(unsigned long magic, unsigned long addr) {
 }
+void writeCharacter(unsigned char c, unsigned char forecolour, unsigned char backcolour, int x, int y)
+{
+     uint16_t attrib = (backcolour << 4) | (forecolour & 0x0F);
+     volatile uint16_t * where;
+     where = (volatile uint16_t *)0xB8000 + (y * 80 + x) ;
+     *where = c | (attrib << 8);
+}
 /* main multiboot entrance */
 void cmain(unsigned long magic, unsigned long addr)
 {
