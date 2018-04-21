d270b9632a38a6a6b9cfb1cae36fbcce22b01426
cth103 <cthdev@email.com>
2018-04-21 21:43:21 +0100

    Create string.h

---
+++ libc/string.h
@@ -0,0 +1,5 @@
+#define NULL ((void *) 0)
+void *memcpy(void *dest, const void *src, size_t count);
+void *memset(void *dest, char val, size_t count);
+unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count);
+size_t strlen(const char *str);
